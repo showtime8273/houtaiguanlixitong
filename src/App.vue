cd5648460d08ea37b8e07e375e3bdb70f4cb15dd
姜宛 <showtime8273.com>
2021-01-26 14:06:56 +0800

    程序员姜宛 主分支第一次保存 包含初始化文件夹

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/login">连接测试</router-link>
+    <router-link to="/home">连接测试</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="loginss">
+    <p>贾斯汀比伯</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+
+</script>
+<style lang="less" scoped>
+</style>
